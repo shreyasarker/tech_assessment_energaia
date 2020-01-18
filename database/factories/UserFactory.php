8d3448a2e94cc1d0deb13ce3f1720a7395b4db88
shreyas10 <shreya.sarker@gmail.com>
2020-01-18 13:31:30 +0600

    User factorymodified

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,7 +1,8 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-use App\User;
+
+use App\Models\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
@@ -18,6 +19,7 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
+        'role_id' => rand(1, 2),
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
