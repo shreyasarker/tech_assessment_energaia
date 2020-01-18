2c39ae459024cda7b232197ecae631455bf7f0b4
shreyas10 <shreya.sarker@gmail.com>
2020-01-18 13:29:49 +0600

    User schema and model modified

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('role_id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('role_id')->references('id')
+                ->on('roles')->onDelete('cascade');
         });
     }
 
