3f616bfb82766f5e34323cce4dca20465cba43b3
shreyas10 <shreya.sarker@gmail.com>
2020-01-18 13:38:41 +0600

    User seeder modified

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $emails = ['admin@email.com', 'supplier@email.com'];
+
+        $user = User::whereIn('email', [$emails])->get();
+
+        if(count($user) <= 0 ){
+            $roles = Role::all();
+
+            factory(User::class)->updateOrCreate([
+                'role_id' => $roles[0]->id,
+                'email' => $emails[0]
+            ]);
+
+            factory(User::class)->updateOrCreate([
+                'role_id' => $roles[0]->id,
+                'email' => $emails[1]
+            ]);
+        }
+    }
+}
