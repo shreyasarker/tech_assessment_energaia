5445989542ca24d3863da098abd1839583344e6f
shreyas10 <shreya.sarker@gmail.com>
2020-01-18 14:50:25 +0600

    Home page added for admin

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    public function index(){
+
+        $data['title'] = 'Home';
+
+        if(Auth::user()->isAdmin()){
+            $data['title'] = 'Admin Dashboard';
+
+        }
+
+        return view('home', $data);
+    }
+}
--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -16,7 +16,7 @@ class AdminMiddleware
      */
     public function handle($request, Closure $next)
     {
-        if(Auth::check() && Auth::user()->isAdmin){
+        if(Auth::check() && Auth::user()->isAdmin()){
             return $next($request);
         }
         return request('/');
